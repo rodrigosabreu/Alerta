1ff544627b3e62722fc34f0d7857bae339aee7ed
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-03 22:08:05 -0200

    App pronto

--- Alerta/ViewController.swift
+++ Alerta/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func abrirAlerta(_ sender: Any) {
+        //criacao do alerta
+        let alerta = UIAlertController(title: "Adicionar contato", message: "Deseja adicionar contato?", preferredStyle: .actionSheet)
+        
+        //botoes - com acao
+        /*let confirmar = UIAlertAction(title: "Confirmar", style: .default) { (acao) in
+            print("Botão confirmar pressionado!")
+        }*/
+        
+        //botoes sem acao - a acao ficará no completion do present
+        let confirmar = UIAlertAction(title: "Confirmar", style: .default, handler:nil)
+        let cancelar = UIAlertAction(title: "Cancelar", style: .destructive, handler:nil)
+        
+        alerta.addAction(confirmar)
+        alerta.addAction(cancelar)
+        
+        //exibir alerta
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
